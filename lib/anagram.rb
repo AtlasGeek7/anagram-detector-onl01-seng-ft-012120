9526d360b1c651c9a5b91b5447ca3a05e6b77e66
Khalid T <khalid.tissire@gmail.com>
2020-02-02 23:41:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(arr)
+    arr.select { |w| w.upcase.bytes.sum === @name.upcase.bytes.sum }
+  end
+end
