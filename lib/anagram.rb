415b1b9d2028117bb66966e20c8bd84a4aa2b322
Khalid T <khalid.tissire@gmail.com>
2020-02-03 01:20:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,17 @@
-
-
 class Anagram
-
   attr_accessor :name
 
   def initialize(word)
-    @name = word 
-  end   
+    @name = word
+  end
 
-  def match(array)
-    array.select {|x| x.split("").sort == @name.split("").sort}
-  end   
+  def ascii_sum(str)
+    sum =  0
+    str.upcase.split("").each { |c| sum += c.ord }
+    return sum
+  end
 
-end 
+  def match(arr)
+    arr.select { |w| ascii_sum(w) === ascii_sum(@name) if (w.size === @name.size) }
+  end
+end
