e3e16b9914f8f77733dd4ba170dfad8d9fced045
Khalid T <khalid.tissire@gmail.com>
2020-02-03 01:19:21 +0000

    Automatically backed up by Learn

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -23,4 +23,16 @@ describe 'Anagram' do
   end
 end
 
+class Anagram
 
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
